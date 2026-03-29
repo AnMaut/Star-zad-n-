90c298224914389c3d3bd79ed09422d2f8ddff5d
AnMaut <mautnerovaan@post.cz>
2026-03-29 11:08:44 +0200

    pytest

--- stare testy/pulsemestralni_test_zadani/midterm_24_E/assignment.py
+++ stare testy/pulsemestralni_test_zadani/midterm_24_E/assignment.py
@@ -3,3 +3,36 @@ SCHOOL_MAX_STUDENTS = [3, 2, 2]
 NEZARAZEN = "nezařazen"
 
 
+def load_schools_data_dict(schools_list):
+    school_dict = {}
+    for school in SCHOOLS:
+        with open(f"{school}.csv", "r") as file:
+            nam_pref_list = []
+            for row in file.readlines()[1:]:
+                nm_prf_tuple = row.strip().split(",")
+                nam_pref_list.append(tuple(nm_prf_tuple))
+
+        school_dict[school] = nam_pref_list
+
+
+
+# ----------------------------------
+# school_dict = {}
+#
+# for school in SCHOOLS:
+#     with open(f"{school}.csv", "r") as file:
+#         nam_pref_list = []
+#         for row in file.readlines()[1:]:
+#             nm_prf_tuple = row.strip().split(",")
+#             nam_pref_list.append(tuple(nm_prf_tuple))
+#             print(nam_pref_list)
+#     school_dict[school] = nam_pref_list
+#     print(school_dict)
+
+
+
+
+
+
+
+
---
+++ stare testy/pulsemestralni_test_zadani/midterm_24_E/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from midterm-24-e!")
+
+
+if __name__ == "__main__":
+    main()
